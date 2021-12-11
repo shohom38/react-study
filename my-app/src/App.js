67574990f23883f375e91f5fed9ab66b440c726b
sumin <shohom38@naver.com>
2021-12-11 10:37:12 +0900

    movie app #1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,41 @@
-import { useState, useEffect } from "react";
+import axios from "axios";
+import React from "react";
+import Movie from "./Movie";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("CALL THE API....");
-  }, []);
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {data: { data :{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });  
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => {
+      console.log(movie);
+      return <Movie key={movie.id} id={movie.id} title={movie.title} year={movie.year} summary={movie.summary} poster={movie.medium_cover_image} />
+    })}</div>;
+  }
 }
+
+// function App() {
+//   const [counter, setValue] = useState(0);
+//   const onClick = () => setValue((prev) => prev + 1);
+//   console.log("i run all the time");
+//   useEffect(() => {
+//     console.log("CALL THE API....");
+//   }, []);
+//   return (
+//     <div>
+//       <h1>{counter}</h1>
+//       <button onClick={onClick}>click me</button>
+//     </div>
+//   );
+// }
 
 export default App;
