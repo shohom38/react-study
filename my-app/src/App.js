f42b0a1f1e5d22780afcff515e8569077673a147
sumin <shohom38@naver.com>
2022-01-22 02:22:03 +0900

    sub #1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
-// import {  }
 function App() {
   return (
     <BrowserRouter>
